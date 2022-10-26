5c115c9f015f3b10359099e7b2ffa870bd16aa40
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-26 22:58:17 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def initialize(value)
+        @name = value
+    end
+    def match arr
+        arr.select {|a| a.split("").sort == @name.split("").sort}
+    end
+end
